a94a232e16c0b4808af00ccd52cf681328f195cb
kvcsPat <smth.patrik@gmail.com>
2024-04-25 16:05:29 +0200

    created StarRating component

--- ui-components/src/App.jsx
+++ ui-components/src/App.jsx
@@ -1,11 +1,13 @@
 import Accordian from "./components/accordian/Accordian";
+import StarRating from "./components/star_rating/StarRating";
 import "./styles_global/reset.css";
 import "./styles_global/global.css";
 
 function App() {
   return (
     <>
-      <Accordian />
+      {/* <Accordian /> */}
+      <StarRating />
     </>
   );
 }
---
+++ ui-components/src/components/star_rating/StarRating.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import "./StarRating.css";
+
+export default function StarRating({ numOfStars = 5 }) {
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
+
+  function handleClick(getCurrentIndex) {
+    setRating(getCurrentIndex);
+  }
+
+  function handleMouseEnter(getCurrentIndex) {
+    setHover(getCurrentIndex);
+  }
+
+  function handleMouseLeave() {
+    setHover(rating);
+  }
+
+  function handleDeleteRating() {
+    setRating(0);
+    setHover(0);
+  }
+
+  return (
+    <div className="star-rating-wrapper">
+      <button onClick={handleDeleteRating} className="star-rating-btn">
+        Delete Rating
+      </button>
+      <div className="star-rating">
+        {[...Array(numOfStars)].map((_, index) => {
+          index += 1;
+          return (
+            <span
+              key={index}
+              className={
+                (index <= (hover || rating) ? "star-active" : "star-inactive") +
+                " " +
+                "material-icons-round"
+              }
+              onClick={() => handleClick(index)}
+              onMouseMove={() => handleMouseEnter(index)}
+              onMouseLeave={handleMouseLeave}
+            >
+              {index <= (hover || rating) ? "star" : "star_border"}
+            </span>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
